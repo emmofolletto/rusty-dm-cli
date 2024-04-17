643f53b1ebb68efdb858df36509edeebb306e665
emmofolletto <emmofolletto@gmail.com>
2024-04-16 18:38:35 -0700

    fix syntax errors

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
-struct {
+use clap::Parser;
+#[derive(Parser)]
+struct Cli {
     pattern: String,
-    path: std::path::PathBuf
+    path: std::path::PathBuf,
 }
 
-
 fn main() {
-    let pattern = std::env::args().nth(1).expect("no pattern given");
-    let path = std::env::args().nth(2).expect("no path given");
-    println!("Hello, adventurer!");
+    let args = Cli::parse();
+    println!(
+        "Hello, adventurer! pattern {:?}, path {:?}",
+        args.pattern, args.path
+    );
 }
